31f55c92e5bb8676d9ee1b502834c1a1683cdb45
Mateus Pessoa Amaral <mateuspa@gmail.com>
2019-06-12 13:54:45 -0300

    Configuração da tela de FAQ's.

---
+++ app/src/main/java/br/com/digitalhouse/digital/pimarvel/menu/listener/RecyclerViewFaqClickListener.java
@@ -0,0 +1,7 @@
+package br.com.digitalhouse.digital.pimarvel.menu.listener;
+
+import br.com.digitalhouse.digital.pimarvel.menu.model.Faq;
+
+public interface RecyclerViewFaqClickListener {
+    void onClick(Faq faq);
+}
--- app/src/main/java/br/com/digitalhouse/digital/pimarvel/menu/view/FaqActivity.java
+++ app/src/main/java/br/com/digitalhouse/digital/pimarvel/menu/view/FaqActivity.java
@@ -1,12 +1,24 @@
 package br.com.digitalhouse.digital.pimarvel.menu.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
-import br.com.digitalhouse.digital.pimarvel.R;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FaqActivity extends AppCompatActivity {
+import br.com.digitalhouse.digital.pimarvel.R;
+import br.com.digitalhouse.digital.pimarvel.menu.adapter.RecyclerViewFaqAdapter;
+import br.com.digitalhouse.digital.pimarvel.menu.listener.RecyclerViewFaqClickListener;
+import br.com.digitalhouse.digital.pimarvel.menu.model.Faq;
+
+public class FaqActivity extends AppCompatActivity implements RecyclerViewFaqClickListener {
+
+    private RecyclerView recyclerView;
+    private RecyclerViewFaqAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +29,29 @@ public class FaqActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*Para listar dinâmicamente todas as perguntas que possam vir a existir,
+        coloquei um Recycler View listando todas elas e a resposta é mostrada no detalhe quando selecionada*/
+        recyclerView = findViewById(R.id.faqRecyclerViewFaqs);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewFaqAdapter(getQuestions(), this);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private List<Faq> getQuestions() {
+        List<Faq> faqs = new ArrayList<>();
+        faqs.add(new Faq("How do I choose a movie?", "Clicar em Filmes na barra de navegação embaixo > Escolher uma das capas > Ponto! Já tem todos os dados do filme selecionado."));
+        faqs.add(new Faq("How do I choose a HQ?", "Clicar em HQs na barra de navegação embaixo > Escolher uma das capas > Ponto! Já tem todos os dados do HQ selecionado."));
+        faqs.add(new Faq("How do I choose an avatar and save on preference?", "Clicar no menu superior > Escolher Settings > Ponto! Escolha o seu herói!"));
+        faqs.add(new Faq("How to play the game?", "Clicar em Game na barra de navegação embaixo > Escolher um herói ou vilão na barra abaixo dos quadros de luta > Ponto! Agora derrote o computador!"));
+        return faqs;
+    }
+
+    @Override
+    public void onClick(Faq faq) {
+        Intent intent = new Intent(this, FaqDetailActivity.class);
+        intent.putExtra("FAQ", faq);
+        startActivity(intent);
     }
 }
