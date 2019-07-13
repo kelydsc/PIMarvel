0c0eb87d257a35486e2cc97577189404ce6d51bf
gustavoshitara <gus_port@yahoo.com.br>
2019-07-13 18:59:19 -0300

    Ajuste Layout Fragment_Movie + Criação do Layout do Detalhe do Movie + Comunicação entre os Fragments de Movie

---
+++ app/src/main/java/br/com/digitalhouse/digital/pimarvel/movie/view/MovieFragmentDetail.java
@@ -0,0 +1,31 @@
+package br.com.digitalhouse.digital.pimarvel.movie.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.digitalhouse.digital.pimarvel.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MovieFragmentDetail extends Fragment {
+
+
+    public MovieFragmentDetail() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_detail, container, false);
+    }
+
+}
